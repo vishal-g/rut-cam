466cc9eff1b9427d70838ae6cb0723f3d3b240b2
Vishal Gaikwad <vishal@tspace.in>
2022-12-22 16:23:47 +0530

    Initial commit

---
+++ wp-content/plugins/rutgers-plugin/rutgers-plugin.php
@@ -0,0 +1,15 @@
+<?php
+ /**
+ * Plugin Name:       TBones Customize
+ * Plugin URI:        https://tappingbones.com
+ * Description:       Custom blocks, code etc.
+ * Version:           1.2
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            Vishaal Gaikwad
+ * Author URI:        https://vishalgaikwad.com/
+ * Text Domain:       rutgers-plugin
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/vishal-g/rutgers-plugin
+ * Primary Branch: main
+ */
