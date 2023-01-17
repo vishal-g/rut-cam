a37cbec2117fa0a6aa4784d3915433b9607d03e9
Vishal Gaikwad <vishal@tspace.in>
2023-01-17 22:55:59 +0530

    Added staff card pattern

---
+++ wp-content/themes/rutgers/patterns/staff-card.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Staff Card
+ * Slug: tbones/staff-card
+ * Categories: featured
+ */
+?>
+<!-- wp:columns {"align":"full","style":{"spacing":{}},"className":"ta-staff-card"} -->
+<div class="wp-block-columns alignfull ta-staff-card"><!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis:50%"><!-- wp:image {"id":54,"sizeSlug":"full","linkDestination":"none"} -->
+        <figure class="wp-block-image size-full"><img src="https://rutgers.test/wp-content/uploads/2022/12/team-member-1.jpg" alt="" class="wp-image-54"/></figure>
+        <!-- /wp:image --></div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"verticalAlignment":"center","width":"50%","className":"ta-staff-card__info-wrapper"} -->
+    <div class="wp-block-column is-vertically-aligned-center ta-staff-card__info-wrapper" style="flex-basis:50%"><!-- wp:heading {"level":4,"className":"ta-staff-card__name"} -->
+        <h4 class="ta-staff-card__name">Marsha Besong, Ed.D.</h4>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"className":"ta-staff-card__title"} -->
+        <p class="ta-staff-card__title">Vice Chancellor for Student Academic Success</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"className":"ta-staff-card__address"} -->
+        <p class="ta-staff-card__address">Armitage Hall, Room 231<br>311 North 5th Street<br>Camden, NJ 08102</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"className":"ta-staff-card__phone"} -->
+        <p class="ta-staff-card__phone">(856) 225-6107</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"className":"ta-staff-card__email"} -->
+        <p class="ta-staff-card__email"><a href="mailto:MW700@camden.rutgers.edu">MW700@camden.rutgers.edu</a></p>
+        <!-- /wp:paragraph --></div>
+    <!-- /wp:column --></div>
+<!-- /wp:columns -->
