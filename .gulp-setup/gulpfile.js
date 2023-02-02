5cd518efc6d6d1aee1b2b3d233e647915f550f03
Vishal Gaikwad <vishal@tspace.in>
2023-02-02 12:24:23 +0530

    working gulper processes

--- .gulp-setup/gulpfile.js
+++ .gulp-setup/gulpfile.js
@@ -5,33 +5,17 @@ const settings = require("./settings.json");
 const themePath = settings.themePath;
 const pluginPath = settings.pluginPath;
 
-const theme = require(`${themePath}/theme.json`);
-
-// Load plugins
-
-// const autoprefixer = require("autoprefixer");
 const browsersync = require("browser-sync").create();
-// const cssnano = require("cssnano");
-// const concat = require('gulp-concat');
-const gulp = require("gulp");
-const sourcemaps = require("gulp-sourcemaps");
-// const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const rename = require("gulp-rename");
 const livereload = require("gulp-livereload");
 var reload = browsersync.reload;
+
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
 const notify = require("gulp-notify");
-
-const postcssImportExtGlob = require("postcss-import-ext-glob");
-const postcssImport = require("postcss-import");
-const postcssNestedAncestors = require("postcss-nested-ancestors");
-const tailwindcssNesting = require("tailwindcss/nesting");
-const postcssExtend = require("postcss-extend");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
 const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-
-var run = require("gulp-run");
-const { exec } = require("child_process");
+const tailwindConfig = require("tailwindcss").Config;
 
 /*############################################################################*/
 // Theme related tasks
@@ -40,62 +24,43 @@ const { exec } = require("child_process");
 // Theme CSS task
 gulp.task("theme-css", () => {
   console.log("inside themecss");
-  return gulp
-    .src(
-      [
-        `${themePath}/assets/src/css/app.pcss`,
-        `${themePath}/assets/src/css/editor-style.pcss`,
-      ],
-      {
-        allowEmpty: true,
-      }
-    )
-    .pipe(sourcemaps.init())
-    .pipe(
-      notify({
-        message: "sourcemaps inited",
-      })
-    )
-    .pipe(
-      postcss([
-        require("postcss-import-ext-glob"),
-        require("postcss-import"),
-        require("postcss-extend"),
-        require("tailwindcss/nesting"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-      ])
-    )
-    .pipe(
-      notify({
-        message: "postcss done",
-      })
-    )
-    .pipe(
-      rename({
-        extname: ".css",
-      })
-    )
-    .pipe(
-      notify({
-        message: ".css renamed",
-      })
-    )
-    .pipe(sourcemaps.write())
-    .pipe(
-      notify({
-        message: "sourcemaps written",
-      })
-    )
-    .pipe(gulp.dest(`${themePath}/assets/build/css`))
-
-    .pipe(
-      notify({
-        message: "files copied to dest css",
-      })
-    )
-    .pipe(browsersync.stream())
-    .pipe(livereload());
+  return (
+    gulp
+      .src(
+        [
+          `${themePath}/assets/src/css/app.pcss`,
+          `${themePath}/assets/src/css/editor-style.pcss`,
+        ],
+        {
+          allowEmpty: true,
+        }
+      )
+      .pipe(sourcemaps.init())
+      // .pipe(
+      //   notify({
+      //     message: "sourcemaps inited",
+      //   })
+      // )
+      .pipe(
+        postcss([
+          require("postcss-import-ext-glob"),
+          require("postcss-import"),
+          require("postcss-extend"),
+          require("tailwindcss/nesting"),
+          tailwindcss(tailwindConfig),
+          require("autoprefixer"),
+        ])
+      )
+      .pipe(
+        rename({
+          extname: ".css",
+        })
+      )
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(`${themePath}/assets/build/css`))
+      .pipe(browsersync.stream())
+      .pipe(livereload())
+  );
 });
 
 // Webfonts task
@@ -112,7 +77,7 @@ gulp.task("theme-images", () => {
     .pipe(gulp.dest(`${themePath}/assets/build/images`));
 });
 
-// Transpile, concatenate and minify scripts
+// Theme js script
 gulp.task("theme-js", () => {
   return gulp
     .src([`${themePath}/assets/src/js/app.js`])
@@ -120,13 +85,6 @@ gulp.task("theme-js", () => {
     .pipe(browsersync.stream())
     .pipe(livereload());
 });
-
-// // theme-json-changed
-// gulp.task("theme-json-changed", () => {
-//   gulp
-//     .watch([`${themePath}/**/*.html`, `${themePath}/theme.json`])
-//     .on("change", reload);
-// });
 
 // == Browser-sync task
 gulp.task("browser-sync", function (done) {
@@ -154,7 +112,6 @@ gulp.task("theme-watch", () => {
     ],
     gulp.series("theme-css")
   );
-  // gulp.watch([`${themePath}/theme.json`], gulp.series("theme-json-changed"));
 
   gulp.watch([`${themePath}/assets/src/js/**/*`], gulp.series("theme-js"));
   gulp.watch(
@@ -230,15 +187,4 @@ gulp.task("plugin-assets-vendors", () => {
 // Common tasks
 /*############################################################################*/
 
-gulp.task(
-  "default",
-  gulp.series(
-    // "copy-postcss-config",
-    "theme-css",
-    "theme-js",
-    "theme-webfonts",
-    "theme-images",
-    "browser-sync",
-    "theme-watch"
-  )
-);
+gulp.task("default", gulp.series("theme-css", "browser-sync", "theme-watch"));
