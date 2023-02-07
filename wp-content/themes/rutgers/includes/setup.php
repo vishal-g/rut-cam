3ad09cca31a0c3ec7e4fabca1dbe431b1ae2a987
Vishal Gaikwad <vishal@tspace.in>
2023-02-07 07:27:50 +0530

    added post-recipe block

--- wp-content/themes/rutgers/includes/setup.php
+++ wp-content/themes/rutgers/includes/setup.php
@@ -3,7 +3,8 @@
 /**
  * Theme setup.
  */
-function tbt_setup() {
+function tbt_setup()
+{
 
 	add_theme_support(
 		'html5',
@@ -17,22 +18,23 @@ function tbt_setup() {
 	);
 
 
-	add_theme_support( 'align-wide' );
-	add_theme_support( 'wp-block-styles' );
+	add_theme_support('align-wide');
+	add_theme_support('wp-block-styles');
 
-	add_theme_support( 'editor-styles' );
-	add_editor_style( 'assets/build/css/editor-style.css' );
+	add_theme_support('editor-styles');
+	add_editor_style('assets/build/css/editor-style.css');
 
-	add_editor_style( 'assets/build/css/app.css' );
+	add_editor_style('assets/build/css/app.css');
 
-	remove_theme_support( 'core-block-patterns' );
+	remove_theme_support('core-block-patterns');
 }
 
 
 // Remove Core Block Patterns
 
-function tbt_block_course_theme_unregister_patterns() {
-	add_filter( 'should_load_remote_block_patterns', '__return_false' );
+function tbt_block_course_theme_unregister_patterns()
+{
+	add_filter('should_load_remote_block_patterns', '__return_false');
 }
 
 
@@ -41,16 +43,16 @@ function tbt_block_course_theme_unregister_patterns() {
 
 // Youtube URL - https://www.youtube.com/watch?v=a1TzCyQ7eUI
 
-function tbt_cc_gutenberg_register_files() {
-	// script file
-	wp_register_script(
-		'cc-block-script',
-		get_stylesheet_directory_uri() .'/assets/build/js/editor-script.js', // adjust the path to the JS file
-		array( 'wp-blocks', 'wp-edit-post' )
-	);
-	// register block editor script
-	register_block_type( 'cc/ma-block-files', array(
-		'editor_script' => 'cc-block-script'
-	) );
+// function tbt_cc_gutenberg_register_files() {
+// 	// script file
+// 	wp_register_script(
+// 		'cc-block-script',
+// 		get_stylesheet_directory_uri() .'/assets/build/js/editor-script.js', // adjust the path to the JS file
+// 		array( 'wp-blocks', 'wp-edit-post' )
+// 	);
+// 	// register block editor script
+// 	register_block_type( 'cc/ma-block-files', array(
+// 		'editor_script' => 'cc-block-script'
+// 	) );
 
-}
+// }
