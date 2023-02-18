f38cf5a457d19462e0b60e896d5d5b7219c5abd0
Vishal Gaikwad <vishal@tspace.in>
2023-02-18 15:40:12 +0530

    post list block some style fixes

--- wp-content/plugins/rutgers-plugin/build/blocks/post-list/index.asset.php
+++ wp-content/plugins/rutgers-plugin/build/blocks/post-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ecea7891a0233fc55fae');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e9f41dad1f30a592b925');
