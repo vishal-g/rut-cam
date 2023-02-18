6b13e4cc3d03d723dde93db341b4db166b522b78
Vishal Gaikwad <vishal@tspace.in>
2023-02-18 22:41:46 +0530

    testimonials slider build files

--- wp-content/plugins/rutgers-plugin/build/blocks/testimonials-slider-item/index.asset.php
+++ wp-content/plugins/rutgers-plugin/build/blocks/testimonials-slider-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f9523a0b868fc37ffabf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '160b57b1d8524b574a32');
