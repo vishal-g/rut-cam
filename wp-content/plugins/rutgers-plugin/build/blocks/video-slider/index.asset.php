b7c3904bc64cd81243b76251d0da90529173ad45
Vishal Gaikwad <vishal@tspace.in>
2023-08-01 00:15:27 +0530

    increasing brightness level of video thumbnails

--- wp-content/plugins/rutgers-plugin/build/blocks/video-slider/index.asset.php
+++ wp-content/plugins/rutgers-plugin/build/blocks/video-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9cc2ddecdd7ea8d508f0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e442e0a31e324eb7aa2b');
