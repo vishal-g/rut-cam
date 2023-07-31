e024bc3ba26bba3e652941a846e474250ef51544
Vishal Gaikwad <vishal@tspace.in>
2023-08-01 01:20:05 +0530

    still brighter

--- wp-content/plugins/rutgers-plugin/build/blocks/video-slider/index.asset.php
+++ wp-content/plugins/rutgers-plugin/build/blocks/video-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e442e0a31e324eb7aa2b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c94dbd3f797860dfea6');
